c62671f2ae3cbb33f39565296d1233b8c8b0942a
Aditya <sinha.aditya244@gmail.com>
2023-01-07 11:46:52 -0800

    basic navbar design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
