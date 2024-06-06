c7d873ee932efcd9ce173db217a9149c6139de93
Aditya <sinha.aditya244@gmail.com>
2024-06-06 19:50:45 +0530

    updated mongo url with env variable

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,9 @@ const commentRoutes = require("./routes/comments");
 const userRoutes = require("./routes/user");
 const subscriptionRoutes = require("./routes/subscribe")
 
-mongoose.connect("mongodb+srv://aditya:V53bkdhA4QHBKB9U@cluster0.eciv35m.mongodb.net/blog?retryWrites=true&w=majority")
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://aditya:V53bkdhA4QHBKB9U@cluster0.eciv35m.mongodb.net/blog?retryWrites=true&w=majority"
+
+mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to database');
     })
