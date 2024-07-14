00f3ce0b1bc72ed7920cf5bea04dbfd27c8f5ca7
Aditya <sinha.aditya244@gmail.com>
2024-07-15 00:02:12 +0530

    Resolved bugs including server crashes
    - resolve console errors on email input in login
    - resolve server crash issues on incorrect pwd and email

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required, Validators.email],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     })
   }
