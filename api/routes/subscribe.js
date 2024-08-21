27d026fb5bdf9fd7b280b52b516eb4d0ec19a415
Aditya <sinha.aditya244@gmail.com>
2024-08-21 16:42:05 +0530

    remove comments js file to resolve server-less function limit

--- api/routes/subscribe.js
+++ api/routes/subscribe.js
@@ -107,3 +107,44 @@ router.get("/get-subscription-updates/:email", (req, res, next) => {
 
 
 module.exports = router;
+
+
+// comment.js file backup
+
+// const express = require("express");
+// const Comment = require('../models/comment');
+// const router = express.Router();
+// const checkAuth = require("../middleware/check-auth");
+
+// router.post("", checkAuth, (req, res, next) => {
+//     const comments = new Comment({
+//         comment: req.body.comment,
+//         blogId: req.body.blogId,
+//         dateOfPublish: req.body.dateOfPublish
+//     });
+//     console.log(req.body)
+//     comments.save();
+//     res.status(201).json({
+//         message: 'Comment added successfully'
+//     })
+// })
+
+// router.get('/:blogId', (req, res, next) => {
+//     Comment.find({blogId:req.params.blogId}).then(documents => {
+//         res.status(200).json({
+//             message: 'Comments fetched succesfully',
+//             comments: documents
+//         })
+//     })
+// })
+
+// router.delete('/:commentId', checkAuth, (req, res, next) => {
+//     Comment.deleteOne({_id: req.params.commentId}).then(result => {
+//         console.log(result);
+//         res.status(200).json({
+//             message: 'Comment deleted successfully'
+//         })
+//     })
+// })
+
+// module.exports = router;
