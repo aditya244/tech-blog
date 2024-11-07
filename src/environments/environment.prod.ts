65097ec03efe70045463f48ce52bed800fb44d3e
Aditya <sinha.aditya244@gmail.com>
2024-11-08 01:22:41 +0530

    Update env.prod file to take from env var from vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://debugtek-backend.vercel.app/api',
-  googleAnalyticsId: 'G-TWGGJN2TD5'
+  googleAnalyticsId: process.env["NEXT_PUBLIC_GOOGLE_ANALYTICS_ID"]
 };
