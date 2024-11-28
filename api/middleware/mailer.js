c3b105b8edb482736bcbb0156ea18bfd85dd3189
Aditya <sinha.aditya244@gmail.com>
2024-11-28 16:26:17 +0530

    without env vars

--- api/middleware/mailer.js
+++ api/middleware/mailer.js
@@ -6,8 +6,10 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS,
+        user: 'info@debugtek.com',
+        pass: 'Aditya@2024dummy'
+        //user: process.env.EMAIL_USER, 
+        //pass: process.env.EMAIL_PASS,
     },
     tls: {
       rejectUnauthorized: false, // Ensure SSL certificate validation
