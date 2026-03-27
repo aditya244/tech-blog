1e85e673b8c3dbe80d885fe56b63c457f3dcfb47
Aditya <sinha.aditya244@gmail.com>
2026-03-28 00:08:35 +0530

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -21,6 +21,7 @@ getTestBed().initTestEnvironment(
 );
 
 // Then we find all the tests.
+// delete this file later
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
